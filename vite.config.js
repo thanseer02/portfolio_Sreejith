632d8f5cb91041e1418fc74c53dcae9a4db67a4d
muhammed tanseer <muhammed.tanseer@mindster.com>
2026-07-16 12:21:47 +0530

    Configure Vite for GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   // Since we are deploying to github pages on a custom domain or a repo path, we use base. 
   // We'll set it to relative path for generic hosting, but for gh-pages repo deployments, 
   // it usually needs to be '/repo-name/'. Relative base './' works for most cases!
-  base: './',
+  base: '/portfolio_Sreejith/',
 })
